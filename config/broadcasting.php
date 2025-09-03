9dfbc38e09df14038ee5d07cc7a64792351aae7e
Dhrigos <dhrigos@gmail.com>
2025-09-04 05:18:46 +0700

    fix reveb channel

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -32,9 +32,9 @@ return [
 
         'reverb' => [
             'driver' => 'reverb',
+            'key' => env('REVERB_APP_KEY'),
+            'secret' => env('REVERB_APP_SECRET'),
             'app_id' => env('REVERB_APP_ID'),
-            'app_key' => env('REVERB_APP_KEY'),
-            'app_secret' => env('REVERB_APP_SECRET'),
             'host' => env('REVERB_HOST', '127.0.0.1'),
             'port' => env('REVERB_PORT', 8080),
             'scheme' => env('REVERB_SCHEME', 'http'),
--- routes/channels.php
+++ routes/channels.php
@@ -1,7 +1,37 @@
 <?php
 
 use Illuminate\Support\Facades\Broadcast;
+use Illuminate\Http\Request;
 
+/*
+|--------------------------------------------------------------------------
+| Broadcast Channels
+|--------------------------------------------------------------------------
+|
+| Here you may register all of the event broadcasting channels that your
+| application supports. The given channel authorization callbacks are
+| used to check if an authenticated user can listen to the channel.
+|
+*/
+
+// User channel (optional - hanya jika menggunakan authentication)
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
+});
+
+// Channel untuk permintaan barang - semua aplikasi dalam grup yang sama dapat akses
+Broadcast::channel('permintaan-barang.{kodeKlinik}', function (Request $request, $kodeKlinik) {
+    // Semua aplikasi dalam grup yang sama dapat akses channel ini
+    // Tidak perlu authentication untuk channel ini
+    return true;
+});
+
+// Channel untuk notifikasi umum
+Broadcast::channel('notifications.{kodeKlinik}', function (Request $request, $kodeKlinik) {
+    return true;
+});
+
+// Channel untuk status update
+Broadcast::channel('status-update.{kodeKlinik}', function (Request $request, $kodeKlinik) {
+    return true;
 });
